b5c7662850584300409c6dc56b04ac91b6f9c5c9
desaizeeshan22 <desaizeeshan22@gmail.com>
2021-12-08 15:35:47 -0600

    added best sum

--- how_sum.py
+++ how_sum.py
@@ -1,23 +1,30 @@
-
-def how_sum(target_sum,numbers,result=[]):
+## Write a function cansum to return an array of  combinations using numbers from the array
+# generating a target sum (numbers maybe repeated) any one combination is the correct answer
+def how_sum(target_sum,numbers,memo={}):
     """numbers (array): array of numbers
     target_sum:int the target sum to be achieved using the above array of numbers"""
-    # key=(target_sum,str(numbers))
-    # if key in memo:
-    #     return memo[key]
+    if target_sum in memo:
+        return memo[target_sum]
     if target_sum<0:
         return None
     if target_sum==0:
         return []
     for elem in numbers:
         remainder=target_sum-elem
-        res=how_sum(remainder,numbers,result)
+        res=how_sum(remainder,numbers,memo)
+        memo[remainder]=res
         if res is not None:
             res.append(elem)
             return res
     return None
 
+##m: target sum
+##n : size of the array
+## Time: O(n^m*m) worst-case without memoization aka brute force approach
+##Space : O(m)
 
-
-
+##memoized
+#Time:O(n*m*m)
+#Space:O(m^2)
 print(how_sum(7,[5,3,4,7]))
+print(how_sum(300,[7,14]))
