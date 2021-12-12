e543ce2cc94d0d04555ae59995927cbc5ef78a7b
desaizeeshan22 <desaizeeshan22@gmail.com>
2021-12-11 21:06:45 -0600

    Added integer to roman numeral conversion

---
+++ int_to_roman.py
@@ -0,0 +1,10 @@
+def intToRoman(self, num):
+    digits = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"),
+              (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"),
+              (5, "V"), (4, "IV"), (1, "I")]
+    result = []
+    for number, symbol in digits:
+        if num == 0: break
+        count, num = divmod(num, number)
+        result.append(symbol * count)
+    return "".join(result)
