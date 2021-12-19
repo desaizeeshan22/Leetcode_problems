5f49112edae34c580c563760c85c20fc58edece4
desaizeeshan22 <desaizeeshan22@gmail.com>
2021-12-19 16:49:33 -0600

    added max profit stocks file

---
+++ max_profit_buy_sell_stocks.py
@@ -0,0 +1,14 @@
+# You are given an array prices where prices[i] is the price of a given stock on the ith day.
+#
+# You want to maximize your profit by choosing a single day to buy one stock and
+# choosing a different day in the future to sell that stock.
+#
+# Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+def maxProfit(prices):
+    curr_min = prices[0]
+    max_profit = 0
+    for i in range(len(prices)):
+        curr_min = min(prices[i], curr_min)
+        max_profit = max(max_profit, prices[i] - curr_min)
+    return max_profit
