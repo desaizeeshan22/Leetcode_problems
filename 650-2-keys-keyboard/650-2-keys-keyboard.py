b5c97988f5183f4f6d97427ae59b0c48005d5c61
Zeeshan Desai <desaizeeshan22@gmail.com>
2022-02-01 00:22:43 -0600

    Time: 16 ms (96.47%), Space: 13.6 MB (12.94%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.py
+++ 650-2-keys-keyboard/650-2-keys-keyboard.py
@@ -1,11 +1,14 @@
 class Solution(object):
     def minSteps(self, n):
-        def min_steps(n):
+        def min_steps(n,memo={}):
+            if n in memo:
+                return memo[n]
             if n==0 or n==1:
                 return 0
             for i in range(n-1,0,-1):
                 if n%i==0:
-                    return n//i+min_steps(i)
+                    memo[n]=n//i+min_steps(i,memo)
+                    return memo[n]
         return min_steps(n)
         """
         :type n: int
