756d4f9e04621ea15ce02337755b94cfc033bbb2
desaizeeshan22 <desaizeeshan22@gmail.com>
2022-02-01 23:44:39 -0600

    added all factors file

---
+++ all_factors.py
@@ -0,0 +1,14 @@
+def all_factors(num):
+    factors=set()
+    i=1
+    while i**2<num:
+        if num%i==0:
+            factors.add(i)
+        i+=1
+    while i>=1:
+        if num % i == 0:
+            factors.add(num//i)
+        i -= 1
+    return factors
+
+print(all_factors(20))
