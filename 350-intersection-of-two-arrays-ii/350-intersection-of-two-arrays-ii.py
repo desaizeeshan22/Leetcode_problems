c06219f343b91b31b235e95bf90e52c9ad97f858
Zeeshan Desai <desaizeeshan22@gmail.com>
2022-02-25 11:49:59 -0600

    Time: 49 ms (53.23%), Space: 13.7 MB (14.79%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
@@ -1,16 +1,19 @@
-from collections import Counter
+
 class Solution(object):
     def intersect(self, nums1, nums2):
-        c1=Counter(nums1)
-        c2=Counter(nums2)
-        res=[]
-        for elem in c1.keys():
-            if elem in c2:
-                i=min(c1[elem],c2[elem])
-                while i>0:
+        def inter(nums1,nums2):
+            if len(nums1)>len(nums2):
+                return inter(nums2,nums1)
+            d1=dict()
+            for elem in nums1:
+                d1[elem]=d1.get(elem,0)+1
+            res=[]
+            for elem in nums2:
+                if d1.get(elem,0)>0:
                     res.append(elem)
-                    i-=1
-        return res
+                    d1[elem]-=1
+            return res
+        return inter(nums1,nums2)
         """
         :type nums1: List[int]
         :type nums2: List[int]
