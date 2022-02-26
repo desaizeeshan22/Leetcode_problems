252f166aa5e4646d6d03b83aa708e353e2fd2e4f
desaizeeshan22 <desaizeeshan22@gmail.com>
2022-02-26 17:05:46 -0600

    added changes to count of distinct elements in windows of size k

--- count_distinct_window.py
+++ count_distinct_window.py
@@ -1,5 +1,6 @@
 ##count distinct elements in contiguous windows of size k for a given array arr
 # eg arr=[10, 20, 20, 10, 30, 40, 10],k=4 ans=[2,3,4,3]
+from collections import Counter
 def window(arr, k):
     res = []
     for i in range(0, len(arr) - k + 1):
@@ -29,10 +30,23 @@ def naive(arr, k):
                 count += 1
         res.append(count)
     return res
-
+def eff_window(arr,k):
+    res=[]
+    first_k=Counter(arr[:k])
+    res.append(len(first_k))
+    for i in range(k,len(arr)):
+        first_k[arr[i-k]]-=1
+        if first_k[arr[i-k]]==0:
+            del first_k[arr[i-k]]
+        first_k[arr[i]]+=1
+        res.append(len(first_k))
+    return res
 
 print(window([10, 20, 20, 10, 30, 40, 10], 4))
 print(window([10, 10, 10, 10], 3))
 
 print(naive([10, 20, 20, 10, 30, 40, 10], 4))
 print(naive([10, 10, 10, 10], 3))
+
+print(eff_window([10,20,20,10,30,40,10],4))
+print(eff_window([10,10,10,10],3))
