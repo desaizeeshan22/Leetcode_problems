7c4f9e61c42abac5ea308c80bd22a083ccad950a
Zeeshan Desai <desaizeeshan22@gmail.com>
2022-09-10 13:39:53 -0500

    Time: 3467 ms (14.13%), Space: 31 MB (33.72%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -1,28 +1,23 @@
-from collections import deque
 class Solution(object):
     def maxSlidingWindow(self, nums, k):
-        output=[]
-        d=deque()
-        max_index=0
-        def clean_deque(deq,i,k,nums):
-            if(deq and deq[0]==i-k):
+        def clean_deque(i,deq):
+            if deq and deq[0]==i-k:
                 deq.popleft()
-            while(deq and nums[i]>nums[deq[-1]]):
-                deq.pop()    
-        for i in range(0,k):
-            clean_deque(d,i,k,nums)
-            d.append(i)
-            if(nums[i]>nums[max_index]):
+            while deq and nums[i]>nums[deq[-1]]:
+                deq.pop()
+        deq=deque()
+        max_index=0
+        for i in range(k):
+            clean_deque(i,deq)
+            deq.append(i)
+            if nums[i]>nums[max_index]:
                 max_index=i
-        output.append(nums[max_index])
+        output=[nums[max_index]]
         for i in range(k,len(nums)):
-            clean_deque(d,i,k,nums)
-            d.append(i)
-            output.append(nums[d[0]])    
+            clean_deque(i,deq)
+            deq.append(i)
+            output.append(nums[deq[0]])
         return output
-
-    
-        
         """
         :type nums: List[int]
         :type k: int
